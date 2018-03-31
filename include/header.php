8f0f203538ac0b75016536e6b9b011a166fd7e4c
Leonardo Biffi <leonardobiffi@outlook.com>
2018-03-31 16:00:14 -0400

    url redirection bug

--- include/header.php
+++ include/header.php
@@ -5,7 +5,7 @@
 
     // If session variable is not set it will redirect to login page
     if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
-      header("location: login.php");
+      header("location: ". BASEURL . "login.php");
       exit;
     }
 
